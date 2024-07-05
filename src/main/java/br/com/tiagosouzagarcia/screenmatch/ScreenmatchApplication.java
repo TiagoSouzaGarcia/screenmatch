a8d764218c1f4e2b4d5983bf5c93eb29b63a79e2
Tiago Souza Garcia <garciats@saude.sc.gov.br>
2024-07-05 17:11:57 -0300

    Interface e Generics

--- src/main/java/br/com/tiagosouzagarcia/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/tiagosouzagarcia/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,9 @@
 package br.com.tiagosouzagarcia.screenmatch;
 
+import br.com.tiagosouzagarcia.screenmatch.model.Caixa;
+import br.com.tiagosouzagarcia.screenmatch.model.DadosSerie;
 import br.com.tiagosouzagarcia.screenmatch.service.ConsumoAPI;
+import br.com.tiagosouzagarcia.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,5 +22,18 @@ public class ScreenmatchApplication implements CommandLineRunner {
 //		System.out.println(json);
 //		json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
+		ConverteDados converteDados = new ConverteDados();
+
+		DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
+		System.out.println(dadosSerie);
+
+		Caixa<String> caixaDeTexto = new Caixa<>();
+		caixaDeTexto.setConteudo("Guardando texto na minha caixa!");
+
+		Caixa<Integer> caixaDeIdade = new Caixa<>();
+		caixaDeIdade.setConteudo(30);
+
+		Caixa<Double> caixaDeValor = new Caixa<>();
+		caixaDeValor.setConteudo(150.50);
 	}
 }
